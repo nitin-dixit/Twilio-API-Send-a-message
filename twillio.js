3491e390ff215c8777265d13956e3caf8330409e
nitinajaydixit <nitinajaydixit@gmail.com>
2022-01-10 18:21:54 +0530

    Add twilio.js

---
+++ twillio.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is a demo of sending messages with twillio api :)',
+     from: '+18529197569',
+     to: '+91797734786'
+   })
+  .then(message => console.log(message.sid));
